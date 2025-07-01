2900c1674b12f78d741256313b2076bcb1651559
egoraleh <yegorzaleh2104@gmail.com>
2025-07-01 10:56:26 +0300

    app init

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2020,
+        sourceType: 'module'
+    },
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {
+        'vue/multi-word-component-names': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn'
+    }
+};
